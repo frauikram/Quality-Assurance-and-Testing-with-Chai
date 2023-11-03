af89563b3c9f1fdd93ce980bf455c7fd7965dd4c
frauikram <119944932+frauikram@users.noreply.github.com>
2023-11-03 16:24:20 +0100

    2_functional-tests.js "Run Functional Tests on an API Response using Chai-HTTP III - PUT method"

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -36,16 +36,22 @@ suite('Functional Tests', function () {
           done();
         });
     });
+    
     // #3
-    test('Send {surname: "Colombo"}', function (done) {
+    // Run Functional Tests on an API Response using Chai-HTTP III - PUT method
+    test('Send {surname: "Colombo"}', function(done) {
       chai
         .request(server)
         .keepOpen()
         .put('/travellers')
-
-        .end(function (err, res) {
-          assert.fail();
-
+        .send({
+          "surname": "Colombo"
+        })
+        .end(function(err, res) {
+          assert.equal(res.status, 200);
+          assert.equal(res.type, "application/json");
+          assert.equal(res.body.name, "Cristoforo");
+          assert.equal(res.body.surname, "Colombo");
           done();
         });
     });
